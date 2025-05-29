65f0c4e011675ad849196474b8ac1f38d1de6f3c
Radleigh Pompei <radleighpompeideveloper@gmail.com>
2025-05-29 08:59:45 +0100

    setup fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use argon2_kdf::{Hasher,Algorithm};
+use rand::Rng;
+use std::fs;
+use std::io::{Read, Write};
+fn setup(password: &[u8], salt_path: &str) {
+    let hash = Hasher::new()
+        .algorithm(Algorithm::Argon2id)
+        .salt_length(16)
+        .hash_length(32)
+        .iterations(8)
+        .memory_cost_kib(2*1024*1024)
+        .threads(4)
+        .hash(password)
+        .unwrap();
+    println!("{:?}", hash.salt_bytes());
+    fs::write(salt_path, hash.salt_bytes()).expect("Failed to write salt");
+}
 fn main() {
-    println!("Hello, world!");
+    let password = b"hello";
+    let salt_path = "salt.bin";
+    setup(password, salt_path);
 }
